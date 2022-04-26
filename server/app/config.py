5f843b4b1c97f34210279708ff805ac0b009630f
windylh <windylhpeng@foxmail.com>
2022-04-26 14:06:22 +0800

    add content length to 128M

--- server/app/config.py
+++ server/app/config.py
@@ -10,7 +10,7 @@ logger = getLogger(__name__)
 
 class Config(object):
     SECRET_KEY = rand_str(32)
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+    MAX_CONTENT_LENGTH = 128 * 1024 * 1024
     REDIS_URL = 'redis://redis:6379/0'
     RATELIMIT_STORAGE_URL = 'redis://redis:6379/1'
 
